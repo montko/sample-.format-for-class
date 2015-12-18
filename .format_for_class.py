cc04cd2a1ce85037ef1d9a2747fbbe88de46c018
Marisa Ontko <montko@gmail.com>
2015-12-17 20:19:07 -0500

    First file

---
+++ .format_for_class.py
@@ -0,0 +1,9 @@
+name_of_student = "Marisa"
+print name_of_student
+
+statement = "Hello {0}, it's nice to meet you".format(name_of_student)
+print statement
+
+color = raw_input("What's your favorite color?")
+response = "Well {0}, {1} is a great color. Nice to meet you.".format(name_of_student, color)
+print response
